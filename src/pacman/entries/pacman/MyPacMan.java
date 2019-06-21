f2524e88437d909418cd9a16667d356f1753a8b4
Giuseppe Mascolo <32833539+giuseppemas@users.noreply.github.com>
2019-06-21 15:30:19 +0200

    Update MyPacMan.java
    modifiche Controller Expectiminimax

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -20,56 +20,60 @@ import java.util.EnumMap;
  * fill in the getAction() method. Any additional classes you write should either
  * be placed in this package or sub-packages (e.g., game.entries.pacman.mypackage).
  */
-public class MyPacMan extends Controller<MOVE>
-{
-	private MOVE myMove=MOVE.NEUTRAL;
-	private int depthResearch = 6;
-	
-	public MOVE getMove(Game game, long timeDue) 
-	{
-		return expectMinMax(game);
-	}
+public class MyPacMan extends Controller<MOVE> {
+    private MOVE myMove = MOVE.NEUTRAL;
+    private int depthResearch = 4;
 
-	private MOVE expectMinMax (Game game){
+    public MOVE getMove(Game game, long timeDue) {
+        return expectMinMax(game);
+    }
 
-		float bestScore = Float.MIN_VALUE;
+    private MOVE expectMinMax(Game game) {
 
-		MOVE[] legalMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
-		MOVE bestMOVE = MOVE.NEUTRAL;
-		for (MOVE move : legalMoves){
-			Game g = game.copy();
-			g.updatePacMan(move);
-			float utility = getMin(g, depthResearch);
-			if (utility>bestScore){
-				bestScore = utility;
-				bestMOVE = move;
-			}
-		}
+        float bestScore = Float.MIN_VALUE;
 
-		return bestMOVE;
-		//System.out.println(numberRecursive);
+        MOVE[] legalMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
+        MOVE bestMOVE = MOVE.NEUTRAL;
+        for (MOVE move : legalMoves) {
+            Game g = game.copy();
+            g.updatePacMan(move);
+            float utility = getMin(g, depthResearch);
+            if (utility > bestScore) {
+                bestScore = utility;
+                bestMOVE = move;
+            }
+        }
 
-	}
+        return bestMOVE;
+        //System.out.println(numberRecursive);
 
-	private float getMin(Game game, int depth ) {
+    }
 
-		if(depth == 0 || game.gameOver() || game.getNumberOfActivePills()+ game.getNumberOfActivePowerPills() ==0){
-			//return (float) Utils.InfluenceFunction(g);
-			return (float) Utils.EvaluationFunction(game);
-		}
+    private float getMin(Game game, int depth) {
 
-		//long s = System.currentTimeMillis();
-		depth=depth-1;
-		//float utility = Float.MAX_VALUE;
-		//float minScore = utility;
-		Game gameMIN = null;
-		float average = 0;
+        if (depth == 0 || game.gameOver() || game.getNumberOfActivePills() + game.getNumberOfActivePowerPills() == 0) {
+            return (float) Utils.InfluenceFunction(game);
+            //return (float) Utils.EvaluationFunction(game);
+        }
 
-		/*ArrayList<MOVE[]> allLegalMoves = new ArrayList<MOVE[]>();
-		for (GHOST ghost: GHOST.values()){
-			allLegalMoves.add(game.getPossibleMoves(game.getGhostCurrentNodeIndex(ghost)));
-		}
-		*/
+        //long s = System.currentTimeMillis();
+        depth = depth - 1;
+        //float utility = Float.MAX_VALUE;
+        //float minScore = utility;
+        Game gameMIN = null;
+        float average = 0;
+
+        ArrayList<MOVE[]> allLegalMoves = new ArrayList<MOVE[]>();
+        for (GHOST ghost : GHOST.values()) {
+			/*if (game.getGhostLairTime(ghost)==0) {
+				allLegalMoves.add(game.getPossibleMoves(game.getGhostCurrentNodeIndex(ghost)));
+			}*/
+            allLegalMoves.add(game.getPossibleMoves(game.getGhostCurrentNodeIndex(ghost)));
+        }
+
+		/*if (allLegalMoves.size()==0){
+			return getMax(game, depth);
+		}*/
 /*		int index = 0;
 		for (MOVE[] moves : allLegalMoves) {
 			if (moves.length == 0){
@@ -79,20 +83,14 @@ public class MyPacMan extends Controller<MOVE>
 			index++;
 		}*/
 
-		//ArrayList<EnumMap<GHOST,MOVE>> combination = getCombination(allLegalMoves);
-		//System.out.println(combination.size());
-		/*
-		if (combination.size()==0){
-			return expectMinMax(g, 0, 0, numberRecursive);
-		}
-
-		for(EnumMap<GHOST,MOVE> ghostState : combination){
-			gameMIN  = game.copy();
-			gameMIN.updateGhosts(ghostState);
-			average+= getMax(gameMIN, depth)/combination.size();
-		}
-		*/
-
+        ArrayList<EnumMap<GHOST, MOVE>> combination = getCombination(allLegalMoves);
+        //System.out.println(combination.size());
+        for (EnumMap<GHOST, MOVE> ghostState : combination) {
+            gameMIN = game.copy();
+            gameMIN.updateGhosts(ghostState);
+            average += getMax(gameMIN, depth) / combination.size();
+        }
+		/* //MIN only For ONE GHOST
 		EnumMap<GHOST,MOVE> mappa = new EnumMap<GHOST, MOVE>(GHOST.class);
 		MOVE[] possibleMOves = game.getPossibleMoves(game.getGhostCurrentNodeIndex(GHOST.BLINKY));
 		for(MOVE mossa : possibleMOves){
@@ -105,63 +103,98 @@ public class MyPacMan extends Controller<MOVE>
 			gameMIN.updateGhosts(mappa);
 			average+= getMax(gameMIN, depth)/possibleMOves.length;
 		}
+		*/
 
-		return average;
+        return average;
 
-	}
+    }
 
-	private float getMax(Game game, int depth) {
-		long s = System.currentTimeMillis();
+    private float getMax(Game game, int depth) {
+        long s = System.currentTimeMillis();
 
-		if(depth == 0 || game.gameOver() || game.getNumberOfActivePills()+ game.getNumberOfActivePowerPills() ==0){
-			//return (float) Utils.InfluenceFunction(g);
-			return (float) Utils.EvaluationFunction(game);
-		}
+        if (depth == 0 || game.gameOver() || game.getNumberOfActivePills() + game.getNumberOfActivePowerPills() == 0) {
+            return (float) Utils.InfluenceFunction(game);
+            //return (float) Utils.EvaluationFunction(game);
+        }
 
-		depth=depth-1;
+        depth = depth - 1;
 
-		float utility = Float.MIN_VALUE;
-		float bestScore = utility;
+        float utility = Float.MIN_VALUE;
+        float bestScore = utility;
 
-		MOVE[] legalMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
+        MOVE[] legalMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
 
-		for (MOVE move : legalMoves){
-			Game gameMAX = game.copy();
-			gameMAX.updatePacMan(move);
-			utility = getMin(gameMAX, depth);
-			if (utility>bestScore){
-				bestScore = utility;
-			}
-		}
+        for (MOVE move : legalMoves) {
+            Game gameMAX = game.copy();
+            gameMAX.updatePacMan(move);
+            utility = getMin(gameMAX, depth);
+            if (utility > bestScore) {
+                bestScore = utility;
+            }
+        }
 
-		return bestScore;
+        return bestScore;
 
-	}
+    }
 
-	private ArrayList<EnumMap<GHOST,MOVE>> getCombination(ArrayList<MOVE[]> legalMoves){
-		//long time = System.currentTimeMillis();
-		ArrayList<EnumMap<GHOST,MOVE>> combination  = new ArrayList<EnumMap<GHOST, MOVE>>();
-		EnumMap<GHOST,MOVE>  moveGHOST = new EnumMap<GHOST, MOVE>(GHOST.class);
+    private ArrayList<EnumMap<GHOST, MOVE>> getCombination(ArrayList<MOVE[]> legalMoves) {
+        //long time = System.currentTimeMillis();
+        ArrayList<EnumMap<GHOST, MOVE>> combination = new ArrayList<EnumMap<GHOST, MOVE>>();
+        EnumMap<GHOST, MOVE> moveGHOST = new EnumMap<GHOST, MOVE>(GHOST.class);
 
-		for (int i = 0; i<legalMoves.get(0).length ; i++){
-			for(int j=0; j<legalMoves.get(1).length; j++){
-				for(int k = 0; k<legalMoves.get(2).length; k++){
-					for(int t=0; t<legalMoves.get(3).length; t++){
+        //System.out.println(legalMoves.size());
+        if (legalMoves.get(0).length == 0) {
+            moveGHOST.put(GHOST.BLINKY, MOVE.NEUTRAL); // BLINKY move
+            moveGHOST.put(GHOST.PINKY, MOVE.NEUTRAL); // PINKY move
+            moveGHOST.put(GHOST.INKY, MOVE.NEUTRAL); // INKY move
+            moveGHOST.put(GHOST.SUE, MOVE.NEUTRAL); // SUE move
 
-						moveGHOST.put(GHOST.values()[0], legalMoves.get(0)[i]); // BLINKY move
-						moveGHOST.put(GHOST.values()[1], legalMoves.get(1)[j]); // PINKY move
-						moveGHOST.put(GHOST.values()[2], legalMoves.get(2)[k]); // INKY move
-						moveGHOST.put(GHOST.values()[3], legalMoves.get(3)[t]); // SUE move
+            combination.add(moveGHOST.clone());
 
-						combination.add(moveGHOST.clone()); //Add combination
+            return combination;
+        } else{
 
-						moveGHOST.clear(); //Remove Elements
+            for (int i = 0; i < legalMoves.get(0).length; i++) {
 
-					}
-				}
-			}
-		}
-		//System.out.println("COMBINATION TIME " + (System.currentTimeMillis()-time));
-		return combination;
-	}
+                if(legalMoves.get(1).length != 0) {
+
+                    for (int j = 0; j < legalMoves.get(1).length; j++) {
+
+                        if (legalMoves.get(2).length != 0) {
+
+                            for (int k = 0; k < legalMoves.get(2).length; k++) {
+
+                                if (legalMoves.get(3).length != 0) {
+                                    for (int t = 0; t < legalMoves.get(3).length; t++) {
+                                        moveGHOST.put(GHOST.BLINKY, legalMoves.get(0)[i]); // BLINKY move
+                                        moveGHOST.put(GHOST.PINKY, legalMoves.get(1)[j]); // PINKY move
+                                        moveGHOST.put(GHOST.INKY, legalMoves.get(2)[k]); // INKY move
+                                        moveGHOST.put(GHOST.SUE, legalMoves.get(3)[t]); // SUE move
+                                        combination.add(moveGHOST.clone()); //Add combination
+                                        moveGHOST.clear(); //Remove Elements
+                                    }
+                                } else {
+                                    moveGHOST.put(GHOST.BLINKY, legalMoves.get(0)[i]); // BLINKY move
+                                    moveGHOST.put(GHOST.PINKY, legalMoves.get(1)[j]); // PINKY move
+                                    moveGHOST.put(GHOST.INKY, legalMoves.get(2)[k]); // INKY move
+                                    combination.add(moveGHOST.clone()); //Add combination
+                                    moveGHOST.clear(); //Remove Elements
+                                }
+                            }
+                        } else {
+                            moveGHOST.put(GHOST.BLINKY, legalMoves.get(0)[i]); // BLINKY move
+                            moveGHOST.put(GHOST.PINKY, legalMoves.get(1)[j]); // PINKY move
+                            combination.add(moveGHOST.clone()); //Add combination
+                            moveGHOST.clear(); //Remove Elements
+                        }
+                    }
+                }else{
+                    moveGHOST.put(GHOST.BLINKY, legalMoves.get(0)[i]); // BLINKY move
+                    combination.add(moveGHOST.clone());
+                    moveGHOST.clear();
+                }
+            }
+        }
+        return combination;
+    }
 }
